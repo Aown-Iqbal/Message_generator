cbd83e28d0c67e8b4b587cca8269f46e305bd3f4
Aown77 <157023973+Aown77@users.noreply.github.com>
2026-01-29 12:39:12 +0500

    Prints time now. And saves error rows

--- message_sender.py
+++ message_sender.py
@@ -5,6 +5,5 @@ def sender(number,message):
     pywhatkit.sendwhatmsg_instantly(number,
                                     message,
                                     wait_time=15+random.randint(1,4),
-                                    
                                     tab_close=True,
                                     close_time=2+random.randint(1,3))
